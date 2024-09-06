5f207d6e7476a4bde661aa375f6f426a2955d78e
Ferida Mohammed <fmohammed@goldengate.cs.brynmawr.edu>
2024-09-05 21:59:36 -0400

    Descriptive message

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,35 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Ferida Mohammed 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  int round, num1, num2, i, sum;
+  int score = 0;
+  
+  printf("Welcome to Math Game!\n");
+  printf("How many rounds do you want to play?\n");
+  scanf("%d", &round);
+
+  for (i = 0; i<round; i++){
+    num1 = rand() % 10;
+    num2 = rand() % 10;
+    printf("%d + %d = ?\n", num1, num2);
+    scanf("%d", &sum);
+    if (sum == (num1 + num2)){
+      printf("Correct!\n");
+      score += 1;
+    }
+    else {
+      printf("Incorrect :(\n");
+    }
+  }
+
+  printf("You answered %d/%d correctly.\n", score, round);
+
   return 0;
 }
