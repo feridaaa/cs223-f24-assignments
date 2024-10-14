bc34e68c9be66cc78dbc6ef068aca98484d0616f
Ferida Mohammed <fmohammed@goldengate.cs.brynmawr.edu>
2024-10-13 22:07:04 -0400

    bitmap complete

--- A06/bitmap.c
+++ A06/bitmap.c
@@ -1,15 +1,28 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Ferida Mohammed
+ * Date: 10/13/2024
+ * Description: reads in a single 64-bit unsigned integer (e.g. unsigned 
+                long) and outputs it as an 8x8 1bpp sprite.
  ---------------------------------------------*/
 #include <stdio.h>
 
 int main() {
   unsigned long img;
+  unsigned long mask = 0x1ul << 63;
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
 
-  // todo: your code here
+  for(int i=0; i<8; i++) {
+    for(int j=0; j<8; j++) {
+      if (img & mask) {
+        printf("@ ");
+      }
+      else {
+        printf("  ");
+      }
+      mask = mask >> 1;
+    }
+    printf("\n");
+  }
   return 0;
 }
