49c8d66ffda6a3ae76f2df90ceed1253c77294b2
Ferida Mohammed <fmohammed@goldengate.cs.brynmawr.edu>
2024-11-02 23:45:28 -0400

    Assignment Complete

--- A08/shell.c
+++ A08/shell.c
@@ -1,5 +1,79 @@
+/*---------------------------------
+ *Author: Ferida Mohammed
+ *Date: 11/1/24
+ *Description: 
+ ---------------------------------*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <errno.h>
+#include <limits.h>
+
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 int main() {
-  return 0;
+    char *input;
+    char cwd[PATH_MAX];
+    
+
+    while (1) {
+        // Get current working directory
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            perror("getcwd() error");
+            continue;
+        }
+
+	// Customize prompt
+        char prompt[PATH_MAX + 20];
+        strcpy(prompt, ANSI_COLOR_CYAN);  
+        strcat(prompt, cwd);              
+        strcat(prompt, " :) ");           
+        strcat(prompt, ANSI_COLOR_RESET); 
+
+        
+        input = readline(prompt);
+
+        // Check if input is NULL or "exit"
+        if (!input || strcmp(input, "exit") == 0) {
+            free(input);
+            break;
+        }
+
+        add_history(input);
+
+        // Split input into command and arguments
+        char *args[100];
+        int i = 0;
+        args[i] = strtok(input, " ");
+        while (args[i] != NULL) {
+            i++;
+            args[i] = strtok(NULL, " ");
+        }
+
+        // Fork and execute the command
+        pid_t pid = fork();
+        if (pid == 0) {           // Child process
+            if (execvp(args[0], args) < 0) {
+                perror("Error executing command");
+            }
+            exit(EXIT_FAILURE);
+        } 
+	else {                  // Parent process: wait for the child to finish
+            wait(NULL);
+        }
+
+	free(input);
+    }
+
+       
+    
+
+    return 0;
 }
--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,34 @@
+/*--------------------------
+ * Author: Ferida Mohammed
+ * Date: 11/1/24
+ * Discription:
+ ---------------------------*/
+
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
+  pid_t pid;
+  printf("%04d] A0\n", getpid());
+  pid = fork();
+
+  if (pid) {   		// parent process
+    printf("%04d] B0\n", getpid());
+
+    pid = fork();
+    if (pid) {   	//parent process
+      printf("%04d] C0\n", getpid()); 
+    }
+    else {		//child process
+      printf("%04d] C1\n", getpid());
+    }
+  }
+
+  else {		// child process
+    printf("%04d] B1\n", getpid());
+  }
+  printf("%04d] Bye\n", getpid());
+
   return 0;
 }
